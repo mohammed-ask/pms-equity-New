262d362c3f48d37b18d8d7d780361f05dda568fa
md ask <mohammedmaheswer12@gmail.com>
2023-12-30 19:03:38 +0530

    Update viewnominee.php

--- main/viewnominee.php
+++ main/viewnominee.php
@@ -1,35 +1,27 @@
 <?php
 include "main/session.php";
-$id = $_GET['hakuna'];
-$rownominee = $obj->selectextrawhere('nominee', "userid=$id and status = 1")->fetch_assoc();
+$rownominee = $obj->selectextrawhere('nominee', "userid=$employeeid and status = 1")->fetch_assoc();
 ?>
 <form id="addfund">
     <div class="row">
         <div class="col-6">
             <label class="form-label mb-0" for="Price">Name</label>
-            <input data-bvalidator="required" class="form-control form-control-sm" id="" value="<?= isset($rownominee['name']) ? $rownominee['name'] : '' ?>" name="name" placeholder="Nominee Name">
+            <input data-bvalidator="required" readonly class="form-control form-control-sm" id="" value="<?= isset($rownominee['name']) ? $rownominee['name'] : '' ?>" name="name" placeholder="Nominee Name">
         </div>
         <div class="col-6">
             <label class="form-label mb-0" for="Price">Date of Birth</label>
-            <input data-bvalidator="required,gap18year" data-bvalidator-msg-gap18year="Nominee Should be minimum 18 year Old" onfocus="setcalenderlimit(this.id,'')" class="form-control form-control-sm" id="date" value="<?= isset($rownominee['name']) ? changedateformatespecito($rownominee['dob'], "Y-m-d", "d/m/Y") : '' ?>" name="dob" placeholder="DOB">
+            <input data-bvalidator="required,gap18year" data-bvalidator-msg-gap18year="Nominee Should be minimum 18 year Old" onfocus="setcalenderlimit(this.id,'')" readonly class="form-control form-control-sm" id="date" value="<?= isset($rownominee['name']) ? changedateformatespecito($rownominee['dob'], "Y-m-d", "d/m/Y") : '' ?>" name="dob" placeholder="DOB">
         </div>
     </div>
     <div class="row mt-2">
         <div class="col-6">
             <label class="form-label mb-0" for="Price">Relation</label>
-            <select data-bvalidator="required" style="width: 100% !important;" class="form-control form-control-sm" id="" name="relation" placeholder="Nominee Relation">
-                <option value="">Select Relation</option>
-                <option value="Mother" <?= isset($rownominee['name']) && $rownominee['relation'] === 'Mother' ? 'selected' : '' ?>>Mother</option>
-                <option value="Father" <?= isset($rownominee['name']) && $rownominee['relation'] === 'Father' ? 'selected' : '' ?>>Father</option>
-                <option value="Children" <?= isset($rownominee['name']) && $rownominee['relation'] === 'Children' ? 'selected' : '' ?>>Children</option>
-                <option value="Spouse" <?= isset($rownominee['name']) && $rownominee['relation'] === 'Spouse' ? 'selected' : '' ?>>Spouse</option>
-                <option value="Brother" <?= isset($rownominee['name']) &&  $rownominee['relation'] === 'Brother' ? 'selected' : '' ?>>Brother</option>
-                <option value="Sister" <?= isset($rownominee['name']) && $rownominee['relation'] === 'Sister' ? 'selected' : '' ?>>Sister</option>
-            </select>
+            <input data-bvalidator="required" readonly class="form-control form-control-sm" id="" value="<?= isset($rownominee['name']) ? $rownominee['relation'] : '' ?>" name="name" placeholder="Nominee Name">
+
         </div>
         <div class="col-6">
             <label class="form-label mb-0" for="Price">Aadhar No.</label>
-            <input data-bvalidator="required,maxlength[12],minlength[12]" value="<?= isset($rownominee['name']) ? $rownominee['adharno'] : '' ?>" class="form-control form-control-sm" id="" name="adharno" placeholder="Aadhar No">
+            <input data-bvalidator="required,maxlength[12],minlength[12]" value="<?= isset($rownominee['name']) ? $rownominee['adharno'] : '' ?>" readonly class="form-control form-control-sm" id="" name="adharno" placeholder="Aadhar No">
             <input type="text" name="userid" hidden value="<?= $id ?>">
         </div>
     </div>
