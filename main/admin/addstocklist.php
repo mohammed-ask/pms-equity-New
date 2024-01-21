a76336eeabafba2c44ea3b5ce6599d5283987018
akshatgroup <marketing.akshatgroup@gmail.com>
2024-01-21 13:23:00 +0530

    Update addstocklist.php

--- main/admin/addstocklist.php
+++ main/admin/addstocklist.php
@@ -5,8 +5,8 @@ include "main/session.php";
 <form id="adduser" onsubmit="event.preventDefault();sendForm('', '', 'insertstocklist', 'resultid', 'adduser');return 0;">
 <div class="row">
     <label class="col-lg-6 col-lg-6 block text-sm  mb-3" style="margin-bottom: 5px;">
-        <span class="text-gray-700 dark:text-gray-400">Stock Name</span>
-        <input name="Symbol" data-bvalidator="required" class="form-control" placeholder="Stock Full Name with Expiry & Strike Price" />
+        <span class="text-gray-700 dark:text-gray-400">Stock Name(with Expiry & Strike Price)</span>
+        <input name="Symbol" data-bvalidator="required" class="form-control" placeholder="BANKNIFTY 25 JAN 2024 CE 36500.00" />
     </label>
     <label class="col-lg-6 col-lg-6 block text-sm  mb-3" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Expiry Date (Delete Automatically on)</span>
