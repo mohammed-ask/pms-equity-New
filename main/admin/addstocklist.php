e1d6a143b50af33b35a169f91723c263e2fb4e1a
akshatgroup <marketing.akshatgroup@gmail.com>
2024-01-21 13:18:57 +0530

    Update addstocklist.php

--- main/admin/addstocklist.php
+++ main/admin/addstocklist.php
@@ -3,7 +3,7 @@ include "main/session.php";
 
 ?>
 <form id="adduser" onsubmit="event.preventDefault();sendForm('', '', 'insertstocklist', 'resultid', 'adduser');return 0;">
-
+<div class="row">
     <label class="col-lg-6 block text-sm  mb-3" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Name</span>
         <input name="Symbol" data-bvalidator="required" class="form-control" placeholder="stock Name" />
@@ -11,7 +11,7 @@ include "main/session.php";
     <label class="col-lg-6 block text-sm  mb-3" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Expiry Date</span>
         <input id="date" data-bvalidator="required" onfocus="setcalenderlimit(this.id,'')" name="expiredate" class="form-control" placeholder="Expire Date" />
-    </label>
+    </label></div>
     <div>
         <button type="submit" id="modalsubmit" class="w-full px-3 py-1 mt-6 text-sm font-medium d-none">
             Submit
