590c20cb0d4526ad33ed0a5454b6b45ebd6de442
akshatgroup <marketing.akshatgroup@gmail.com>
2024-04-01 18:39:41 +0530

    Update dashboard.php

--- main/dashboard.php
+++ main/dashboard.php
@@ -263,7 +263,7 @@ if ($dashboardmaintanance) {
                 <div class="col-12 mb-4">
                     <div class="card">
                         <a href="portfolio">
-                            <img width="100%" src="main/dist/userstuff/assets\img\dashboard\ai-trading-dashboard.gif" alt="AI Trade" class="rounded" /></a>
+                            <img width="100%" src="main/dist/userstuff/assets\img\dashboard\ai-trading-dashboard.gif" alt="AI Trade" style="border-radius:0.5rem;" /></a>
 
                     </div>
                 </div>
